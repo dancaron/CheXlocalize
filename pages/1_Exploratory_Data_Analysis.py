e65671618c55a83df5bf118c974d4783eac18cf8
Kinal Patel <136503460+kinal1999@users.noreply.github.com>
2023-06-23 16:18:51 -0500

    Update 1_Exploratory_Data_Analysis.py

--- pages/1_Exploratory_Data_Analysis.py
+++ pages/1_Exploratory_Data_Analysis.py
@@ -12,7 +12,7 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "CheXlocalize"))
 
-from count_segs import count_segs
+from CheXlocalize.count_segs import count_segs
 
 st.markdown("## Exploratory Data Analysis")
 st.divider()
